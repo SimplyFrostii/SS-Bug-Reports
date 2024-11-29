7a8467a1c59e3d54b6f5455cf258311a8d605cbc
Dylan.M <73547524+SimplyFrostii@users.noreply.github.com>
2024-11-29 16:19:51 +0000

    clean palette

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('nav ul li a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        alert('Navigating to ' + event.target.textContent);
-    });
-});
